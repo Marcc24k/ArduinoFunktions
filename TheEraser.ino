d1ace22b3336653b965d31d6ff0cba29faf8d525
Marcc24k <marcc24k@gmail.com>
2015-11-22 18:13:59 -0500

    Update TheEraser.ino

--- TheEraser.ino
+++ TheEraser.ino
@@ -1,23 +1,21 @@
 //We always have to include the library
 #include "LedControl.h"
-
 /*
- Now we need a LedControl to work with.
+
 
  pin 12 is connected to the DataIn 
  pin 11 is connected to the CLK 
  pin 10 is connected to LOAD 
- We have only a single MAX72XX.
+
  */
 LedControl lc=LedControl(12,11,10);
 
-/* we always wait a bit between updates of the display */
+/* display update */
 unsigned long delaytime=100;
 
 void setup() {
   /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
+   Wake up MAX!
    */
   lc.shutdown(0,false);
   /* Set the brightness to a medium values */
